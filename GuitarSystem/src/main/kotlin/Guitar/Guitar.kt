f217bd68df0bcbf16f6323b0f08b851819bcc031
= <=>
2024-03-19 23:55:04 +0530

    Add guitarSpecs attribute to Guitar class

--- GuitarSystem/src/main/kotlin/Guitar/Guitar.kt
+++ GuitarSystem/src/main/kotlin/Guitar/Guitar.kt
@@ -2,8 +2,10 @@ package org.example.Guitar
 
 class Guitar(
     private val serialNumber: Int,
-    private val price: Double
+    private val price: Double,
+    private val guitarSpec: GuitarSpec
 ) {
-        public fun getSerialNumber() = serialNumber
-    fun getPrice() = price
+    public fun getSerialNumber() = serialNumber
+    public fun getPrice() = price
+    public fun getGuitarSpec() = guitarSpec
 }
--- GuitarSystem/src/test/kotlin/GuitarTests.kt
+++ GuitarSystem/src/test/kotlin/GuitarTests.kt
@@ -1,4 +1,8 @@
 import org.example.Guitar.Guitar
+import org.example.Guitar.GuitarSpec
+import org.example.enums.Builder
+import org.example.enums.Type
+import org.example.enums.Wood
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -7,12 +11,13 @@ class GuitarTests {
     @Test
     fun `should get the serial number and price of a guitar`(){
 
-            val guitar = Guitar(101,23.0)
-            val serialNumber = guitar.getSerialNumber()
-            val price = guitar.getPrice()
+        val guitarSpec = GuitarSpec(Builder.FENDER,"model", Type.ACOUSTIC, Wood.CEDAR, Wood.ALDER)
+        val guitar = Guitar(101,23.0,guitarSpec)
+        val serialNumber = guitar.getSerialNumber()
+        val price = guitar.getPrice()
 
-            assertEquals(101,serialNumber)
-            assertEquals(23.0,price)
+        assertEquals(101,serialNumber)
+        assertEquals(23.0,price)
     }
 
 }
